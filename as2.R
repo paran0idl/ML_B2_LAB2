bae8c944297704e1e778e0480485314dfb0e93a8
paran0idl <lizu0817@me.com>
2019-12-11 13:07:49 +0100

    utf shit

--- as2.R
+++ as2.R
@@ -11,9 +11,8 @@ test=data[-id,]
 
 x<-t(train[,-4703])
 
-#y<-train[[4703]]
-y<-as.factor(train[,4703])
-y
+y<-t(train[[4703]])
+
 
 x_test<-t(train[,-4703])
 
@@ -31,28 +30,42 @@ pamr.plotcv(cvmodel)
 pamr.predict(mod,my_data_test$x,threshold = thr)
 res$yhat
 pamr.plotcen(mod,my_data,thr)
-pamr.listgenes(mod,my_data,thr)
-
+listgene<-pamr.listgenes(mod,my_data,thr)[1:10]
+my_data$genenames[as.numeric(listgene)]
 library(glmnet)
 
 x<-train[,-4703]
-y<-train[,4703]
+y<-train[[4703]]
 
 x_test<-test[,-4703]
 y_test<-test[[4703]]
 
 mod<-cv.glmnet(as.matrix(x),y,alpha=0.5,family="binomial")
+mod$lambda.1se
 penalty_min<-mod$lambda.min
+penalty_min
 real_mod<-glmnet(as.matrix(x),y,alpha=0.5,lambda = penalty_min,family="binomial")
 predict(real_mod,as.matrix(x_test),type="class")
 
 
+library(kernlab)
+
+data <- read_delim("data/data.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+data<-read.csv("data/data.csv")
+data
+x=data[,-4703]
+y=data[[4703]]
+y
+fit<-ksvm(x,y,kernel="vanilladot")
+
+fit
+
 library(e1071)
 mod<-svm(Conference~.,data=train,kernel="vanilladot")
 summary(mod)
-pred<-predict(mod,test[,-4703],type="class")
+pred<-predict(mod,test[,-4703])
 pred
 
 
-
+t<-t.test(Conference~.,data=train)
 
